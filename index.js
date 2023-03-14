c4076ebe71d4aaaa8e05c2bbb3a31291acedeb23
richard miruka onsare <richardonsare15@gmail.com>
2023-03-15 00:11:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+ // declare variables
+const num1 = 31;
+const num2 = 2;
+const num3 = 16;
+const num4 = 6;
+
+// multiply num1 and num2
+const multiply = num1 * num2;
+
+// generate a random integer between 1 and 100
+const random = Math.floor(Math.random() * 100) + 1;
+
+// calculate remainder of num3 divided by num4
+const mod = num3 % num4;
+
+// find the highest number in the set
+const max = Math.max(5, 10, 15, 20);
+
+// test the values
+console.log(multiply); // output: 62
+console.log(random); // output: random integer between 1 and 100
+console.log(mod); // output: 4
+console.log(max); // output: 20
+
